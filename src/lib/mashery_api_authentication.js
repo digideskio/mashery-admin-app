73e7536178f52748b74073807a55ed46060c19eb
Jeremy Pollock <jpollock@tibco.com>
2015-12-08 16:50:53 -0800

    updating auth approach

--- src/lib/mashery_api_authentication.js
+++ src/lib/mashery_api_authentication.js
@@ -3,11 +3,12 @@ var request = require('request')
 
 exports = module.exports = function authenticate(req,res,next) {
     var post_data = {
-        'area_id' : req.body.areaId,
         'area_uuid': req.body.areaUuid,
-        'username': req.user.id
-    };   
+        'username': req.user.id,
+        'password': req.body.password
+    };
     request.post({url:'http://' + process.env.WEBDB_PORT_5432_TCP_ADDR + ':' + process.env.WEBDB_PORT_5432_TCP_PORT + '/authentication', form: post_data}, function(err,httpResponse,body) { 
+        console.log(body);
         if (httpResponse.statusCode == 200) {
             var mashery_access_token = JSON.parse(body);
             req.session.mashery_access_token = mashery_access_token
